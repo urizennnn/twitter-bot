bc06b458189f133ef5ec6e20da5e7ad17907bb21
urizennnn <igamerryt@gmail.com>
2024-07-16 19:03:42 +0100

    refactor

--- server.ts
+++ server.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import logger from 'morgan';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -11,6 +13,6 @@ app.get('/', (req, res) => {
 	res.send('Hello World');
 });
 
-app.listen(3000, () => {
-	console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+	console.log(`${`Server is running on port ${PORT}`}`);
 });
