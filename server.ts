26c1af40e13549391d112baccc492f77967e0697
urizennnn <igamerryt@gmail.com>
2024-07-16 19:01:24 +0100

    Initial commit

---
+++ server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import logger from 'morgan';
+
+const app = express();
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+app.listen(3000, () => {
+	console.log('Server is running on port 3000');
+});
---
+++ twitter/api.ts
@@ -0,0 +1,5 @@
+import { TwitterApi } from 'twitter-api-v2'
+const client = new TwitterApi({
+	clientId: 'YOUR_API,',
+	clientSecret: 'YOUR_API'
+})
